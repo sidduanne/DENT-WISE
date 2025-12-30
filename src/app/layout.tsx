5b145ded4ac90f4a5b96efc7e37ffb317987364f
sidduanne <siddarthaanne@gmail.com>
2025-12-30 15:27:27 +0530

    appointments page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
 } from '@clerk/nextjs'
 import UserSync from "@/components/UserSync";
 import TanStackProvider from "@/components/providers/TanStackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,6 +48,7 @@ export default function RootLayout({
         <html lang="en">
           <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
           <UserSync />
+          <Toaster />
           {children}
           </body>
         </html>
